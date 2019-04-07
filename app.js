dc46767f550fdb7ecb83b665695922b9532b7110
bvabhijeeth1999 <42472199+bvabhijeeth1999@users.noreply.github.com>
2019-04-07 23:52:10 +0530

    make it better

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(bodyParser.urlencoded({extended : false}));
 const mongoOptions = {useNewUrlParser:true};
 const router = express.Router();
 const db = require('./db');
-const port = process.env.PORT
+const port = process.env.PORT || 5000
 
 
 
